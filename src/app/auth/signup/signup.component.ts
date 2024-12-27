ddba55bb84f11c2bd8cdfefec6ddb754ba22f755
DESKTOP-5G35IDM\Jesus Marquez <jesmqz@gmail.com>
2024-12-27 10:20:38 -0500

    Form Arrays

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -34,6 +34,11 @@ export class SignupComponent {
     'role': new FormControl('student', {
       validators: [Validators.required]
     }),
+    source: new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false),
+    ]),
     'agree': new FormControl(false, { validators: [Validators.required] }),
     
   });
