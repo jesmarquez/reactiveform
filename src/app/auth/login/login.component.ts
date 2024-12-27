80df53ccdbc742610200599bcd38082fef120d5d
DESKTOP-5G35IDM\Jesus Marquez <jesmqz@gmail.com>
2024-12-27 07:19:56 -0500

    Interacting with the form programmatically

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { of } from 'rxjs';
+import { debounceTime, of } from 'rxjs';
 
 function mustContainQuestionMark(control: AbstractControl) {
   if (control.value.includes('?')) {
@@ -24,7 +24,8 @@ function emailIsUnique (control: AbstractControl) {
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private destroyRef =  inject(DestroyRef);
   form = new FormGroup({
     email: new FormControl('', {
       validators: [Validators.email, Validators.required ],
@@ -51,7 +52,28 @@ export class LoginComponent {
     );
   }
 
+  ngOnInit() {
 
+    // const savedForm = window.localStorage.getItem('saved-login-form');
+
+    // if (savedForm) {
+    //   const loadedForm = JSON.parse(savedForm);
+    //   this.form.patchValue({
+    //     email: loadedForm.email,
+
+    //   });
+    // }
+    const subscription = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
+      next: value => {
+        window.localStorage.setItem(
+          'saved-login-form', JSON.stringify({ email: value.email })
+        );
+      }
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+
+  }
   onSubmit() {
     console.log(this.form);
     const enteredEmail = this.form.value.email;
